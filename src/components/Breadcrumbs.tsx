27fa1215671b4a509e44c95ee32aeff7e3ecad1c
Reina Ben <rlyn@Reinas-MacBook-Air.local>
2024-03-06 08:42:36 -0800

    complete auto breadcrumb feature

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,9 +1,48 @@
+import { useRouter } from 'next/router'
+
 export default function Breadcrumbs() {
+  const router = useRouter()
+  const urlPath = router.asPath.split('/')
+
+  const gotoPath = (visitPath: string) => {
+    const visitPathIndex = urlPath.indexOf(visitPath)
+    let pathString = ''
+
+    // build url path and
+    // call router.push()
+
+    urlPath.forEach((path, pathIndex) => {
+      if (pathIndex <= visitPathIndex) {
+        if (pathIndex === 1) {
+          pathString += path
+        } else {
+          pathString += '/'+path
+        }
+        
+      }
+    })
+
+    router.push(pathString)
+  }
+
   return <div className="text-sm breadcrumbs">
     <ul>
-      <li><a>Home</a></li> 
-      <li><a>Documents</a></li> 
-      <li>Add Document</li>
+      {
+        urlPath.map((path, index) =>
+          <li
+            key={path}
+            onClick={() => gotoPath(path)}
+          >
+            {
+              path === ''
+            ?
+              'Home'
+            :
+              path.charAt(0).toUpperCase() + path.slice(1)
+            }
+          </li>
+        )
+      }
     </ul>
   </div>
 }
